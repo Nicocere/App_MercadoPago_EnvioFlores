584ca87ff8f41c1cda0d70b692d27f5fb3652b7d
Nicocere <103131425+Nicocere@users.noreply.github.com>
2022-11-08 12:57:05 -0300

    Add files via upload

---
+++ Services/PaymentService.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+class PaymentService {
+  async createPayment(req) {
+
+    console.log("SERVICEE", req)
+
+    let itemsCart = req.bodyMp
+    console.log(itemsCart)
+
+    const url = "https://api.mercadopago.com/checkout/preferences";
+    const body = {
+      payer_email: req.mail,
+      items: itemsCart,
+      back_urls: {
+        failure: "/failure",
+        pending: "/pending",
+        success: "/success"
+      }
+    };
+    const payment = await axios.post(url, body, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+      }
+    });
+    return payment.data;
+  }
+}
+module.exports = PaymentService;
---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+const PaymentController = require("../Controllers/PaymentsController");
+const PaymentService = require("../Services/PaymentService");
+
+const PaymentInstance = new PaymentController(new PaymentService());
+
+router.get("/", function (req, res, next) {
+  return res.json({
+    "/payment": "generates a payment link",
+    "/subscription": "generates a subscription link"
+  });
+});
+
+router.post("/payment", function (req, res, next) {
+
+  console.log("REQ", req.body)
+
+  PaymentInstance.getPaymentLink(req.body, res);
+});
+
+
+module.exports = router;
