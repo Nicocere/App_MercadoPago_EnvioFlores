9948a5d0975eae541ef0d2ecf805c23f68742164
Nicocere <nm.cerecedo@outlook.com>
2022-11-11 16:08:42 -0300

    gitignore

--- api/api.js
+++ api/api.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 const PaymentController = require("../Controllers/PaymentsController");
 const PaymentService = require("../Services/PaymentService");
@@ -15,7 +15,7 @@ router.get("api-mp.vercel.app/", function (req, res, next) {
 
 router.post("/payment", function (req, res, next) {
 
-  console.log("REQ", req.body)
+ 
 
   PaymentInstance.getPaymentLink(req.body, res);
 });
