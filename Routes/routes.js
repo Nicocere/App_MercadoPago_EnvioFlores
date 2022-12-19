a54c764d5ab538d064a614f7d53c9c969f72d3c8
Nicocere <nm.cerecedo@outlook.com>
2022-12-19 12:36:36 -0300

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -8,12 +8,12 @@ const PaymentInstance = new PaymentController(new PaymentService());
 
 router.get("/", function (req, res, next) {
   return res.json({
-    ".payment": "generates a payment link",
+    "/payment": "generates a payment link",
     "/subscription": "generates a subscription link"
   });
 });
 
-router.post(".payment", function (req, res, next) {
+router.post("/payment", function (req, res, next) {
 
   console.log("REQ", req.body)
 
