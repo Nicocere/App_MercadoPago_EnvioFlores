ffa19aac0824badcfb0f825a1f3afdbdb30310bd
Nicocere <nm.cerecedo@outlook.com>
2022-12-27 11:38:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ const app = express();
 app.options('*', cors())
 app.use(cors())
 
-// app.use(function (req, res, next) {
+app.use(function (req, res, next) {
 
-//     res.header('Access-Control-Allow-Origin', "http://localhost:4200");
-//     res.header('Access-Control-Allow-Headers', true);
-//     res.header('Access-Control-Allow-Credentials', true);
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-//     next();
-//   });
+    res.header('Access-Control-Allow-Origin', "http://localhost:4200");
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+  });
 
 
 dotenv.config();
