82c0bfeb707d93253d6a7ecd842d5cba1e031c7f
Nicocere <nm.cerecedo@outlook.com>
2022-12-27 10:15:16 -0300

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -8,10 +8,6 @@ const PaymentInstance = new PaymentController(new PaymentService());
 
 router.get("/", function (req, res, next) {
 
-  console.log(" esto es RES", res)
-  console.log("este es el REQ", req)
-  console.log("ahora pasaa el NEXT", next)
-
   return res.json(
     {
     "/payment": "generates a payment link",
@@ -23,7 +19,7 @@ router.get("/", function (req, res, next) {
 
 router.post("/payment", function (req, res, next) {
   
-  res.redirect('https://mp-envioflores.vercel.app/payment')
+  // res.redirect('https://mp-envioflores.vercel.app/payment')
   console.log("REQ", req.body)
 
   PaymentInstance.getPaymentLink(req.body, res);
